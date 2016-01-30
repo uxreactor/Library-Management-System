0f950124802325548ff3fe6708a25535b7a9fa03
Vijaya Prabhakar Konduri <vkonduri@uxreactor.org>
2016-01-30 16:58:24 +0530

    Login redirection with session variables
    Fixed #5 Login form link is not working and #3 Create session variables
    for admin and member logins

--- code/controller/validateLogin.php
+++ code/controller/validateLogin.php
@@ -2,10 +2,25 @@
 	require 'controller.php';
 	$email = $_POST['email'];
 	$password = $_POST['password'];
+	$type = $_POST['type'];
+	
 	$message=login($email,$password);
 	if ($message) {
-		header('Location:our-library.html');
-	}else{
-		echo $message;
+		echo setSession($type,$email);
+	} else {
+		echo 0;
+	}
+
+
+	function setSession($type,$email){
+		session_start();
+		if($type == 'admin'){
+			$_SESSION["admin"] = $email;
+			$_SESSION["type"] = "admin";
+		}else{
+			$_SESSION["user"] = $email;
+			$_SESSION["type"] = "user";
+		}
+		return $_SESSION["type"];
 	}
 ?>
