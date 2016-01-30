9535b09cf84b9fd4f0f622b356408ca2418ef27d
Vijaya Prabhakar Konduri <vkonduri@uxreactor.org>
2016-01-30 17:15:50 +0530

    Fixed #6
    There was an additional script tag which was causing the error

--- code/login.php
+++ code/login.php
@@ -48,65 +48,64 @@
 
     <?php include ("javascript-links.php");?> 
     <script>
-    var submitForm = function() {
-        var validation_message;
-        var type = $('.nav-tabs li.active a').text().toLowerCase().trim();
-        if(type == 'admin'){
-            login_details = {
-            email:$('#email').val(),
-            password:$('#password').val(),
-            type: type          
-            };
-            login = [{ type : 'email' , value: $('#email').val() , errorMessage:'Email is required' }, { type:'password' , value: $('#password').val() , errorMessage:'Password is required' }];
-        }else {
-            login_details = {
-            email:$('#email2').val(),
-            password:$('#password2').val(),
-            type: type          
-            }; 
-            login = [{ type : 'email' , value: $('#email2').val() , errorMessage:'Email is required' }, { type:'password' , value: $('#password2').val() , errorMessage:'Password is required' }];
-        }
+        var submitForm = function() {
+            var validation_message;
+            var type = $('.nav-tabs li.active a').text().toLowerCase().trim();
+            if(type == 'admin'){
+                login_details = {
+                email:$('#email').val(),
+                password:$('#password').val(),
+                type: type          
+                };
+                login = [{ type : 'email' , value: $('#email').val() , errorMessage:'Email is required' }, { type:'password' , value: $('#password').val() , errorMessage:'Password is required' }];
+            }else {
+                login_details = {
+                email:$('#email2').val(),
+                password:$('#password2').val(),
+                type: type          
+                }; 
+                login = [{ type : 'email' , value: $('#email2').val() , errorMessage:'Email is required' }, { type:'password' , value: $('#password2').val() , errorMessage:'Password is required' }];
+            }
 
-        validation_message = validateForm(login);          
-         console.log(login_details);
-        if(submitToServer(validation_message)){
-            //alert(login_details);
-            $.ajax({             
-                url: $('form').attr('action'),
-                type: $('form').attr('method'),
-                data: login_details,
-                success: function(response){
-                    console.log(response);
-                    alert(response);
-                    //redirect to dashboard
-                    if(response == 'admin'){
-                        window.location.href = "our-library.php";
-                    }else if(response == 'user'){
-                        window.location.href = "my-books.php";
-                    }else{
-                        //Show login failure
+            validation_message = validateForm(login);          
+             console.log(login_details);
+            if(submitToServer(validation_message)){
+                //alert(login_details);
+                $.ajax({             
+                    url: $('form').attr('action'),
+                    type: $('form').attr('method'),
+                    data: login_details,
+                    success: function(response){
+                        console.log(response);
+                        //redirect to dashboard
+                        if(response == 'admin'){
+                            window.location.href = "our-library.php";
+                        }else if(response == 'user'){
+                            window.location.href = "member.php";
+                        }else{
+                            //Show login failure
+                        }
+                    },
+                    error: function(xhr, desc, err){
+                        console.log(desc);
                     }
-                },
-                error: function(xhr, desc, err){
-                    console.log(desc);
+                });
+            } 
+            else{
+                if (login_details.type == 'admin') {
+
+                    body = document.getElementById('admin_email');
+                    writeError(validation_message[0],body);
+                    body = document.getElementById('admin_password');
+                    writeError(validation_message[1],body);
+
+                }else{
+
+                    body = document.getElementById('user_email');
+                    writeError(validation_message[0],body);
+                    body = document.getElementById('user_password');
+                    writeError(validation_message[1],body);
                 }
-            });
-        } 
-        else{
-            if (login_details.type == 'admin') {
-
-                body = document.getElementById('admin_email');
-                writeError(validation_message[0],body);
-                body = document.getElementById('admin_password');
-                writeError(validation_message[1],body);
-
-            }else{
-
-                body = document.getElementById('user_email');
-                writeError(validation_message[0],body);
-                body = document.getElementById('user_password');
-                writeError(validation_message[1],body);
-
             }
             return false;
         }
