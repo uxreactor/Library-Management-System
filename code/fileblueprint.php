02bddaed75e13ce9c33a9eb151be3b9f21d6d1b5
mohanbabburi <mgoud@uxreactor.org>
2016-01-30 13:11:03 +0530

    Footer.
    Changes made in the footer file and added javascript links and
    sample.php file file blueprint page.

---
+++ code/fileblueprint.php
@@ -0,0 +1,10 @@
+<?php include "header-admin.php" ?>
+<?php include "footer.php" ?>
+
+<?php include "javascript-links.php" ?>
+
+<script type="text/javascript">
+	//javascript 
+</script>
+</body>
+</html>
---
+++ code/javascript-links.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/lightbox.min.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>   
+<script type="text/javascript" src="js/script.js"></script>
