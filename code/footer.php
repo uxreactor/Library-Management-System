c1fac900f862c023aff0b28e976bde5d0738db2d
balanarsimharaotanneeru <btanneeru@uxreactor.org>
2016-01-30 12:41:02 +0530

    Header and Footer files.
    These files contain seperate files of header and footer with php
    extension. Headers are again subdivided based on the users.

---
+++ code/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <footer>
+        <div class="container">
+            <div class="col-sm-12">
+                <div class="copyright-text text-center">
+                    <p>&copy; UXReactor. All Rights Reserved.</p>
+                    <p>Designed and Developed by <a target="_blank" href="http://www.uxreactor.com">UXReactor Coding Batch 01</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/#footer-->
+</html>
