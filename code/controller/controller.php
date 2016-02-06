4e72100d3e5050caf635db9e4be0867089ee53b1
nagalakshmiyarra <nyarra@uxreactor.org>
2016-02-06 09:52:24 +0530

    order of books table is changed

--- code/controller/controller.php
+++ code/controller/controller.php
@@ -355,13 +355,13 @@
 		    	$row_isbn = $result_isbn->fetch_assoc();
 				$object = array();
 		    	$object['ISBN'] = $row["isbn"];
-		    	$object['Price'] = $row["price"];
+		    	$object['Book name'] = $row["book_name"];
+		    	$object['Author name'] = $row["author_name"];
 		    	$object['Edition'] = $row["edition"];
 		    	$object['Publisher'] = $row["publisher"];
 		    	$object['Category'] = $row["category"];
-		    	$object['Book name'] = $row["book_name"];
-		    	$object['Author name'] = $row["author_name"];
 		    	$object['Quantity'] = $row_isbn["COUNT(isbn)"];
+		    	$object['Price'] = $row["price"];
 		    	$object['Action'] = "Edit,Delete";
 		    	array_push($arrayObject, $object);
 		    }
@@ -396,13 +396,13 @@
 		    	$row_isbn = $result_isbn->fetch_assoc();
 				$object = array();
 		    	$object['Isbn'] = $row["isbn"];
-		    	$object['Price'] = $row["price"];
+		    	$object['Book name'] = $row["book_name"];
+		    	$object['Author name'] = $row["author_name"];
 		    	$object['Edition'] = $row["edition"];
 		    	$object['Publisher'] = $row["publisher"];
 		    	$object['Category'] = $row["category"];
-		    	$object['Book name'] = $row["book_name"];
-		    	$object['Author name'] = $row["author_name"];
 		    	$object['Quantity'] = $row_isbn["COUNT(isbn)"];
+		    	$object['Price'] = $row["price"];
 		    	array_push($arrayObject, $object);
 		    }
 		} else {
